f94f386dd718a8a0d4ca8efd58697585c6a724c4
phantom <koo011707@gmail.com>
2024-10-30 16:26:10 -0700

    copied&modified necessary parts from old RobotHardware & teleop.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopPIDMain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopPIDMain.java
@@ -2,48 +2,19 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.Gamepad;
-import com.qualcomm.robotcore.hardware.ServoImplEx;
-import com.qualcomm.robotcore.util.ElapsedTime;
 
-@TeleOp(name="pidding")
+@TeleOp(name="teleport operations")
 public class TeleopPIDMain extends LinearOpMode {
-    DcMotorEx LF;
-    DcMotorEx LB;
-    DcMotorEx RF;
-    DcMotorEx RB;
-//    DcMotorEx leftlift = hardwareMap.get(DcMotorEx.class, "leftlift");
-//    DcMotorEx rightlift = hardwareMap.get(DcMotorEx.class, "rightlift");
-//    ServoImplEx claw = hardwareMap.get(ServoImplEx.class, "claw");
-
-
+    robot robo;
     @Override
     public void runOpMode() {
-        LF = hardwareMap.get(DcMotorEx.class, "LF");
-        RF = hardwareMap.get(DcMotorEx.class, "RF");
-        LB = hardwareMap.get(DcMotorEx.class, "LB");
-        RB = hardwareMap.get(DcMotorEx.class, "RB");
-        telemetry.addLine("Ready for start!");
-        telemetry.update();
-
-        double error;
-        double output;
-
-        double currentPos;
-
-        waitForStart();
-
-        Gamepad currentGamepad = new Gamepad();
-        Gamepad previousGamepad = new Gamepad();
-
-        while (opModeIsActive()) {
-            previousGamepad.copy(currentGamepad);
-            currentGamepad.copy(gamepad1);
-            ElapsedTime timer = new ElapsedTime();
-
-            //chassis
+        while (opModeInInit()){
+            telemetry.addLine("starting");
+            telemetry.update();
+            robo = new robot(this);
+            robo.init();
+        }
+        while (opModeIsActive()){
             double y = -gamepad1.right_stick_y;
             double x = gamepad1.right_stick_x;
             double rx = gamepad1.left_stick_x;
@@ -54,18 +25,15 @@ public class TeleopPIDMain extends LinearOpMode {
             double lf = (y + x + rx) / denominator;
             double lb = (y - x + rx) / denominator;
 
-            RF.setPower(rf);
-            RB.setPower(rb);
-            LF.setPower(lf);
-            LB.setPower(lb);
+            robo.setMotorPowers(rf, rb, lf, lb);
 
-//            if (gamepad1.left_bumper) {leftlift.setPower(1);rightlift.setPower(1);}
-//            else if (gamepad1.right_bumper) {leftlift.setPower(-1);rightlift.setPower(-1);}
-//            else {leftlift.setPower(0);rightlift.setPower(0);}
+            if (gamepad1.left_trigger>.1){
+                robo.lift.setPower(.5);
+            }
+            if (gamepad1.right_trigger>.1){
+                robo.lift.setPower(-.5);
+            }
 
-//            if(gamepad1.x){claw.setPosition(1);}
-            telemetry.addData("rx",gamepad1.right_stick_x);
-            telemetry.update();
         }
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot.java
@@ -0,0 +1,115 @@
+/* Copyright (c) 2022 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.ServoImplEx;
+
+/**
+ * This file works in conjunction with the External Hardware Class sample called: ConceptExternalHardwareClass.java
+ * Please read the explanations in that Sample about how to use this class definition.
+ *
+ * This file defines a Java Class that performs all the setup and configuration for a sample robot's hardware (motors and sensors).
+ * It assumes three motors (left_drive, right_drive and arm) and two servos (left_hand and right_hand)
+ *
+ * This one file/class can be used by ALL of your OpModes without having to cut & paste the code each time.
+ *
+ * Where possible, the actual hardware objects are "abstracted" (or hidden) so the OpMode code just makes calls into the class,
+ * rather than accessing the internal hardware directly. This is why the objects are declared "private".
+ *
+ * Use Android Studio to Copy this Class, and Paste it into your team's code folder with *exactly the same name*.
+ *
+ * Or.. In OnBot Java, add a new file named RobotHardware.java, drawing from this Sample; select Not an OpMode.
+ * Also add a new OpMode, drawing from the Sample ConceptExternalHardwareClass.java; select TeleOp.
+ *
+ */
+@Config
+public class robot {
+    public LinearOpMode myOpMode;
+    public OpMode notMyopMode;
+    public DcMotorEx leftFront, leftBack, rightFront, rightBack, lift;/*might not exit? , leftLift, rightLift*/;
+//    public ServoImplEx claw;
+
+    public robot(LinearOpMode opmode) {
+        myOpMode = opmode;
+    }
+    public robot(OpMode opmode) {
+        notMyopMode = opmode;
+    }
+
+
+    public void init() {
+//        claw = myOpMode.hardwareMap.get(ServoImplEx.class, "_");
+        lift = myOpMode.hardwareMap.get(DcMotorEx.class, "lift");
+//        leftLift = myOpMode.hardwareMap.get(DcMotorEx.class, "rightLift");
+//        rightLift = myOpMode.hardwareMap.get(DcMotorEx.class, "rightLift");
+        leftFront = myOpMode.hardwareMap.get(DcMotorEx.class, "leftFront");
+        leftBack = myOpMode.hardwareMap.get(DcMotorEx.class, "leftBack");
+        rightFront = myOpMode.hardwareMap.get(DcMotorEx.class, "rightFront");
+        rightBack = myOpMode.hardwareMap.get(DcMotorEx.class, "rightBack");
+
+
+        leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        lift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+ //       rightLift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+
+        setMotorPowers(0);
+//        FrontLeft.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+//        FrontRight.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+//        BackLeft.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+//        BackRight.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        lift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//        leftLift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//        rightLift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+    }
+
+    public void setMotorPowers(double speed) {
+        leftFront.setPower(speed);
+        leftBack.setPower(speed);
+        rightFront.setPower(speed);
+        rightBack.setPower(speed);
+    }
+    public void setMotorPowers(double s1, double s2, double s3, double s4) {
+        leftFront.setPower(s1);
+        leftBack.setPower(s2);
+        rightFront.setPower(s3);
+        rightBack.setPower(s4);
+    }
+
+}
