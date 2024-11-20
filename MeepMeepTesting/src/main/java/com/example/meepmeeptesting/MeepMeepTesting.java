2d6f71452dcd873c7b30febe153658539329d66d
phantom <koo011707@gmail.com>
2024-11-19 17:18:30 -0800

    very very very rough auto(uncolored)

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -8,19 +8,40 @@ import org.rowlandhall.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 public class MeepMeepTesting {
     public static void main(String[] args) {
+        Pose2d red1 = new Pose2d(10,70,Math.toRadians(-90));
+        Pose2d red2 = new Pose2d(-10,70,Math.toRadians(-90));
+        Pose2d blue1 = new Pose2d(10,-70,Math.toRadians(-90));
+        Pose2d blue2 = new Pose2d(-10,70,Math.toRadians(-90));
         MeepMeep meepMeep = new MeepMeep(800);
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
-                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
-                        .forward(30)
+                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(red1)
+                        .forward(45)
+                        //claw thing
                         .turn(Math.toRadians(90))
-                        .forward(30)
+                        .forward(35)
                         .turn(Math.toRadians(90))
-                        .forward(30)
+                        //picker the upper :)))))
+                        .forward(35)
+                        //putter the downer
+                        .back(35)
+                        .turn(Math.toRadians(-90))
+                        .forward(10)
+                        //picker the upper again
                         .turn(Math.toRadians(90))
-                        .forward(30)
+                        .forward(35)
+                        //putter the downer again
+                        .back(35)
+                        .turn(Math.toRadians(-90))
+                        .forward(10)
+                        //picker the upper again again
                         .turn(Math.toRadians(90))
+                        .forward(35)
+                        //putter the downer again again
+                        .back(50)
+                        .turn(Math.toRadians(90))
+                        .forward(35)
                         .build());
 
 
