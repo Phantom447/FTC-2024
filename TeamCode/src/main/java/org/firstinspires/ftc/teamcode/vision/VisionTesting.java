7ea657755082c786b029659b190862ace0f5ab2a
phantom <koo011707@gmail.com>
2024-12-12 17:47:41 -0800

    vision setup

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/VisionTesting.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode.vision;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvWebcam;
+
+@Autonomous(name="VisionTest")
+public class VisionTesting extends LinearOpMode {
+
+    public void runOpMode(){
+        OpenCvWebcam webcam;
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened()
+            {
+                webcam.startStreaming(960, 720, OpenCvCameraRotation.UPRIGHT);
+            }
+            @Override
+            public void onError(int errorCode)
+            {/*o*/}
+        });
+        while (opModeInInit()) {
+            //x
+        }
+        while (opModeIsActive() && !isStopRequested()) {
+            //x
+        }
+    }
+}
